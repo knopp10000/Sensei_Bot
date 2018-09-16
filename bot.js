13b5156ea97b34258cd2b82d2fef8d91f5605983
knopp10000 <karl.gustafsson@live.com>
2018-09-16 14:35:06 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,81 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+     if(message.substring(0, 21) == '<@490604527796486184>'){
+       var args = message.substring(21).split(' ');
+       var cmd = args[1];
+       logger.info('cmd: ' + cmd + ' args: ' + args)
+
+       //args = args.splice(1);
+       switch(cmd.toLowerCase()) {
+           // !ping
+           case 'ping':
+               bot.sendMessage({
+                   to: channelID,
+                   message: 'Pong!'
+               });
+           break;
+           case 'admin':
+               bot.sendMessage({
+                   to: channelID,
+                   message: '<@!294957314207645696> är admin!'
+               });
+           break;
+           case 'visa':
+           switch(args[2].toLowerCase()){
+             case 'schema':
+                   bot.sendMessage({
+                       to: channelID,
+                       message: 'https://daisy.dsv.su.se/servlet/schema.moment.Momentschema?id=6458'
+                     });
+             break;
+             default:
+               bot.sendMessage({
+                   to: channelID,
+                   message: 'jag vet tyvärr inte vad du menar med: ' + args[2] + ' :(('
+                 });
+                 break;
+           }
+           case 'slå':
+            switch(args[2].toLowerCase()){
+              case 'tärning':
+                let dicerole = Math.floor(Math.random()*6);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: user + ' slog en ' + dicerole + '!'
+                      });
+              break;
+              // default:
+              //   bot.sendMessage({
+              //       to: channelID,
+              //       message: 'jag vet tyvärr inte vad du menar med: ' + args[2] + ' :('
+              //     });
+              //     break;
+            }
+          // default:
+          //   bot.sendMessage({
+          //       to: channelID,
+          //       message: 'jag vet tyvärr inte vad du menar med: ' + args[1] + ' :('
+          //     });
+          //     break;
+         }
+
+     }
+});
